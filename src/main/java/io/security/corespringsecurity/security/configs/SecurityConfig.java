23b7d2e0888a8f463b75d3bc7869bd25c9aa66ba
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-02-10 16:48:22 +0900

    feat(server): 사용자 생성 및 리소스 권한 부여

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -12,13 +18,49 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    public UserDetailsManager users() {
+
+        String password = passwordEncoder().encode("1111");
+
+        UserDetails user = User.builder()
+                .username( "user" )
+//                .password("{noop}1111")
+                .password( password )
+                .roles( "USER" )
+                .build();
+
+        UserDetails manager = User.builder()
+                .username("manager")
+                .password( password )
+                .roles("MANAGER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password( password )
+                .roles("ADMIN", "MANAGER", "USER")
+                .build();
+
+        return new InMemoryUserDetailsManager( user, manager, admin );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
+                        .antMatchers("/").permitAll()
+                        .antMatchers("/mypage").hasRole("USER")
+                        .antMatchers("/messages").hasRole("MANAGER")
+                        .antMatchers("/config").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .formLogin();
-        
+
 
         return http.build();
     }
