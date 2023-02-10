2adfadd2f7ef56307ec31bd74bd7fb21f12a2dc2
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-02-10 17:35:27 +0900

    feat: 사용자 생성 Controller 구현

--- src/main/java/io/security/corespringsecurity/controller/user/UserController.java
+++ src/main/java/io/security/corespringsecurity/controller/user/UserController.java
@@ -1,16 +1,46 @@
 package io.security.corespringsecurity.controller.user;
 
 
+import io.security.corespringsecurity.domain.Account;
+import io.security.corespringsecurity.domain.AccountDto;
+import io.security.corespringsecurity.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
-	
-	@GetMapping(value="/mypage")
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@GetMapping(value = "/mypage")
 	public String myPage() throws Exception {
 
 		return "user/mypage";
+	}
+
+	@GetMapping("/users")
+	public String createUser() {
+
+		return "user/login/register";
+	}
+
+	@PostMapping("/users")
+	public String createUser(AccountDto accountDto) {
+
+		// modelmapper 라이브러리를 사용한다.
+		ModelMapper modelMapper = new ModelMapper();
+		Account account = modelMapper.map(accountDto, Account.class);
+		account.setPassword(passwordEncoder.encode(account.getPassword()));
+		userService.createUser(account);
+
+		return "redirect:/";
 	}
 }
